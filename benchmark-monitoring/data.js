a696d0941fe07347fdd74bec3fac72493674730f
github-action-benchmark <github@users.noreply.github.com>
2022-03-08 06:41:02 +0000

    add C++ Benchmark (googlecpp) benchmark result for 8b6c9602a279c520d6aa8e6f374592be71b6d827

--- benchmark-monitoring/data.js
+++ benchmark-monitoring/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646721225571,
+  "lastUpdate": 1646721662387,
   "repoUrl": "https://github.com/ChuanqiXu9/async_simple",
   "entries": {
     "C++ Benchmark": [
@@ -144,6 +144,42 @@ window.BENCHMARK_DATA = {
             "value": 7805223.3068180885,
             "unit": "ns/iter",
             "extra": "iterations: 88\ncpu: 7802156.818181818 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yedeng.yd@linux.alibaba.com",
+            "name": "Chuanqi Xu",
+            "username": "ChuanqiXu9"
+          },
+          "committer": {
+            "email": "yedeng.yd@linux.alibaba.com",
+            "name": "Chuanqi Xu",
+            "username": "ChuanqiXu9"
+          },
+          "distinct": true,
+          "id": "8b6c9602a279c520d6aa8e6f374592be71b6d827",
+          "message": "Split Performance CI",
+          "timestamp": "2022-03-08T14:39:41+08:00",
+          "tree_id": "2a93f8732aab7416e6cb35d45c8b866ac6112a32",
+          "url": "https://github.com/ChuanqiXu9/async_simple/commit/8b6c9602a279c520d6aa8e6f374592be71b6d827"
+        },
+        "date": 1646721661199,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "async_simple_Lazy_chain",
+            "value": 23638.42851337632,
+            "unit": "ns/iter",
+            "extra": "iterations: 29530\ncpu: 23636.688113782595 ns\nthreads: 1"
+          },
+          {
+            "name": "async_simple_Lazy_collectAll",
+            "value": 11349420.759259487,
+            "unit": "ns/iter",
+            "extra": "iterations: 54\ncpu: 11348974.074074076 ns\nthreads: 1"
           }
         ]
       }
